c28dc9061cf5b6426ae2c8b9b7815cde2eb275a0
Elliot Foster <efoster@firetaco.com>
2014-03-16 14:58:51 -0700

    can expect(ModelA).to.haveOne(ModelB)

--- chai-bookshelf.js
+++ chai-bookshelf.js
@@ -1,2 +1,45 @@
-module.exports = function() {
+var _          = require('lodash')
+  , inflection = require('inflection')
+;
+
+module.exports = function(chai, utils) {
+  chai.Assertion.addMethod('haveOne', function(expected) {
+    var obj = this._obj;
+
+    this.assert(
+      'function' === typeof(obj.forge)
+      , 'relationship subject should be a bookshelf model class'
+    );
+
+    this.assert(
+      'function' === typeof(expected.forge)
+      , 'relationship expectation should be a bookshelf model class'
+    );
+
+    var inst = obj.forge({});
+    var table = _.result(expected.prototype, 'tableName');
+
+    this.assert(
+      table
+      , 'relationship expectation does not have a tableName'
+    );
+
+    var relationName = inflection.singularize(table);
+    var relationship = inst.related(relationName);
+
+    // assert that there is a relationship
+    this.assert(
+      relationship
+      , "model classes have no relation"
+    );
+
+    var data         = relationship.relatedData;
+    var type         = data.type;
+    var target       = data.target;
+
+    this.assert(
+      type == 'hasOne'
+      , "expected a 'hasOne' relationship instead of '" + type + "'"
+    );
+  });
 };
--- test/chai-bookshelf.js
+++ test/chai-bookshelf.js
@@ -1,11 +1,130 @@
 var chai      = require('chai')
   , expect    = chai.expect
   , subject   = require('../')
-  , bookshelf = require('bookshelf')
+  , Bookshelf = require('bookshelf')
+  , inspect
+  , db
+  , Class
+  , School
+  , Slacker
+  , Student
+  , Transcript
 ;
 
 describe("chai-bookshelf", function() {
-  it("can be used by chai without error", function() {
+  before(function() {
+    chai.use(function (chai, utils) {
+      inspect = utils.objDisplay;
+
+      chai.Assertion.addMethod('fail', function (message) {
+        var obj = this._obj;
+
+        new chai.Assertion(obj).is.a('function');
+
+        try {
+          obj();
+        } catch (err) {
+          this.assert(
+              err instanceof chai.AssertionError
+            , 'expected #{this} to throw a chai exception, but it threw ' + inspect(err));
+          this.assert(
+              err.message === message
+            , 'expected #{this} to fail with ' + inspect(message) + ', but got ' + inspect(err.message));
+          return;
+        }
+
+        this.assert(false, 'expected #{this} to fail, but no exception was thrown');
+      });
+    });
+
     chai.use(subject);
+  });
+
+  beforeEach(function() {
+    this.c = {}; // context that is rebuild between tests
+
+    db = Bookshelf.initialize({
+      client: 'sqlite'
+      , connection: {
+        filename: ':memory:'
+      }
+    });
+
+    // student hasOne transcript
+    // school  hasMany students
+    // class belongsTo school
+    // student belongsToMany classes
+
+    Class = db.Model.extend({
+      tableName: 'classes'
+      , school: function() {
+        return this.belongsTo(School);
+      }
+    });
+
+    Transcript = db.Model.extend({
+      tableName: 'transcripts'
+    });
+
+    Student = db.Model.extend({
+      tableName: 'students'
+      , transcript: function() {
+        return this.hasOne(Transcript);
+      }
+    });
+
+    School = db.Model.extend({
+      tableName: 'schools'
+    });
+
+    Slacker = db.Model.extend({});
+  });
+
+  describe(".haveOne", function() {
+    context("when given two related models", function() {
+      it("passes", function() {
+        expect(Student).to.haveOne(Transcript);
+      });
+    });
+
+    context("when given two unrelated models", function() {
+      it("raises an error", function() {
+        expect(function(){
+          expect(Transcript).to.haveOne(Class);
+        }).to.fail("model classes have no relation");
+      });
+    });
+
+    context("when given two models with a non-hasOne relation", function() {
+      it("raises an error", function() {
+        expect(function(){
+          expect(Class).to.haveOne(School);
+        }).to.fail("expected a 'hasOne' relationship instead of 'belongsTo'");
+      });
+    });
+
+    context("when given a class subject and a non-class expectation", function() {
+      it("raises an error", function() {
+        expect(function(){
+          expect(Class).to.haveOne('not a bookshelf model class');
+        }).to.fail("relationship expectation should be a bookshelf model class");
+      });
+    });
+
+    context("when related class does not have a table defined", function() {
+      it("raises an error", function() {
+        expect(function(){
+          expect(Class).to.haveOne(Slacker);
+        }).to.fail("relationship expectation does not have a tableName");
+      });
+    });
+
+    context("when given a non-class subject and a non-class expectation", function() {
+      it("raises an error", function() {
+        expect(function(){
+          expect('not a bookshelf model class').to.haveOne(School);
+        }).to.fail("relationship subject should be a bookshelf model class");
+      });
+    });
   });
 });
